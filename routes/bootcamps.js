d3e5b9f3202fe9e8af22b2f7db600338a33c87e3
Shisan Nafiz <shisannafiz8@gmail.com>
2020-04-05 11:49:57 -0400

    first commit

---
+++ routes/bootcamps.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+  getBootcampsInRadius,
+  bootcampPhotoUpload
+} = require('../controllers/bootcamps');
+
+const Bootcamp = require('../models/Bootcamp');
+
+//Include other resource routers
+const courseRouter = require('./courses');
+const reviewRouter = require('./reviews');
+
+const router = express.Router();
+
+const advancedResults = require('../middleware/advancedresults');
+const { protect, authorize } = require('../middleware/auth');
+
+//Reroute into other resource routers
+router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewRouter);
+
+router
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(protect, authorize('publisher', 'admin'), createBootcamp);
+
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+  .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
+
+router
+  .route('/:id/photo')
+  .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
+
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
+
+module.exports = router;
